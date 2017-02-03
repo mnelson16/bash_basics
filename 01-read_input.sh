d15a0d9c50f3a0e5565fae566c7752a00cfbd58d
Matthew Nelson <mnelson16@ggc.edu>
2017-02-03 13:34:12 -0500

    I allowed the user to create a new text file of their name choice.

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -10,3 +10,8 @@ echo 'Your name was stored in $name'
 
 # exercise: write a script that asks the user for a 
 # filename and create an empty file named after it
+
+echo "Hello, name! What file would you like to create?"
+read filename
+echo > $filename.txt
+echo "You've created $filename.txt"
